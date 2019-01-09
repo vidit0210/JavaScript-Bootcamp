e5882cc245550a998a187851d39f3c925fa84332
Vidit Shah <vidit0210@gmail.com>
2019-01-09 19:18:56 +0530

    logicalOperation

--- JavaScriptBasics/logical-and-or.js
+++ JavaScriptBasics/logical-and-or.js
@@ -1,4 +1,17 @@
-let tempInFarenheight = 55;
-if (temp >= 60 && temp <= 60) {
+let temp = 55;
+if (temp >= 60 && temp <= 90) {
   console.log("It is Pretty Nice out");
+}
+if (temp >= 55 || temp <= 90) {
+  console.log("Do not go Outside");
+}
+let isGuestOneVegan = false;
+let isGuestTwoVegan = false;
+
+if (isGuestOneVegan && isGuestTwoVegan) {
+  console.log("Set Vegan Menu");
+} else if (isGuestOneVegan || isGuestTwoVegan) {
+  console.log("One of them is Vegan,Make Arrangements");
+} else {
+  console.log("No Vegan Arrangements Needed");
 }
