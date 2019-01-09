61130e95ba0738cf5881cecebe0289e797b5f95b
Vidit Shah <vidit0210@gmail.com>
2019-01-09 12:50:56 +0530

    Challenge

---
+++ Challenge/1.js
@@ -0,0 +1,5 @@
+let city = "Mumbai";
+let country = "India";
+let location = city + ", " + country;
+
+console.log(location);
--- JavaScriptBasics/strings.js
+++ JavaScriptBasics/strings.js
@@ -1,3 +1,9 @@
 //create a Variable  using double quotes or single quotes.
-let name = "Vidit";
-console.log(name);
+// let name = "Vidit";
+// console.log(name);
+//Automatic Semilcon Insertion
+let firstName = "Vidit";
+let lastName = "Shah";
+//+ is concatenation
+let fullName = firstName + " " + lastName;
+console.log(fullName);
