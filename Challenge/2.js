89ca528ad729431d75eee4f165c6ffb97d35b747
Vidit Shah <vidit0210@gmail.com>
2019-01-09 14:33:41 +0530

    numbers

---
+++ Challenge/2.js
@@ -0,0 +1,4 @@
+let noOfCorrectAnswer = 10;
+let totalNumberOfQuestions = 100;
+let percentage = (noOfCorrectAnswer / totalNumberOfQuestions) * 100;
+console.log(percentage);
---
+++ JavaScriptBasics/number.js
@@ -0,0 +1,12 @@
+let num = 34 - -3;
+let x = 11;
+let opperation = x + 1 * 2; //13
+let operation2 = (x + 1) * 2; //24
+// console.log(num);
+// console.log(opperation);
+// console.log(operation2);
+
+//convert human age to dog years
+let age = 26;
+let dogYears = (age + 1) / 7;
+//console.log(dogYears);
