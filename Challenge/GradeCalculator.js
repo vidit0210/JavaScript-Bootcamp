76644b0190a07930b411fddbaa4735e941523a69
Vidit Shah <vidit0210@gmail.com>
2019-01-22 16:06:43 +0530

    GradeCalculator

---
+++ Challenge/GradeCalculator.js
@@ -0,0 +1,17 @@
+//Students Score/ Maximum Score
+
+let checkGrade = function (precent) {
+    if (precent >= 90) return 'A';
+    if (precent >= 80 && precent <= 89) return 'B'
+    if (precent >= 70 && precent <= 79) return 'C'
+    if (precent >= 60 && precent <= 69) return 'D'
+    if (precent <= 59) return 'F'
+
+}
+let getReport = function (obtain_marks, totalMarks) {
+    let precent = (obtain_marks / totalMarks) * 100;
+    let grade = checkGrade(precent)
+    return grade
+}
+
+console.log(getReport(5, 20));
--- Function-2/3.js
+++ Function-2/3.js
@@ -1,7 +1,13 @@
 //Arguments
 //Multiple Arguments
 
-let getScoreText = function (name, score = 0) {
-    console.log(name)
-    console.log(score)
+let getScoreText = function (name = 'Anonymous', score = 0) {
+    return `Player ${name} scored ${score}`
 }
+
+//Challenge
+let getTip = function (amount, tip = 0.2) {
+    return tip * amount;
+}
+
+console.log((getScoreText("Vidit", 90)))
