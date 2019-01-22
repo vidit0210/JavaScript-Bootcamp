1679a6cb8755242b4cd9effea1ab2887e4413a22
Vidit Shah <vidit0210@gmail.com>
2019-01-22 23:02:00 +0530

    number-methods

---
+++ Objects/number-methods.js
@@ -0,0 +1,9 @@
+let num = 104.561
+console.log(num.toFixed(2))
+console.log(Math.round(num))
+console.log(Math.floor(num))
+console.log(Math.ceil(num))
+let min = 10
+let max = 20
+let randomNum = Math.floor(Math.random() * (max - min + 1)) + min
+console.log(randomNum)
